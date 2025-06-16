ab6e06d5fcb0d94d0f4ca35e60c6efdab4ce8ee3
zingrin <jerinloncheu@gmail.com>
2025-06-16 10:39:56 +0600

    Display logo as image in body with increased size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Slider from "../components/Slider";
 import Faq from "./Faqs";
 import StudentFeedback from "../components/StudentFeedback";
 import Achievements from "../components/Achievements";
+import Courses from "../components/Courses";
 
 const Home = () => {
   return (
     <div>
       <Slider></Slider>
+      <Courses></Courses>
       <StudentFeedback></StudentFeedback>
       <Achievements></Achievements>
       <Faq></Faq>
